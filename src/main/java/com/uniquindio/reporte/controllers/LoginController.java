e1f19bacc5758ffda2c85bf682f45a78b61f15f1
Jacobo Vargas <jacobo.vargas@cofincafe.com>
2025-04-12 07:51:57 -0500

    Validation that the account is active at login request and configure implementations of gradle

--- src/main/java/com/uniquindio/reporte/controllers/LoginController.java
+++ src/main/java/com/uniquindio/reporte/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.uniquindio.reporte.controllers;
 import com.uniquindio.reporte.model.DTO.loggin.LoginRequestDto;
 import com.uniquindio.reporte.model.DTO.loggin.TokenDTO;
 import com.uniquindio.reporte.service.impl.LoginServiceImpl;
+import com.uniquindio.reporte.utils.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,8 @@ public class LoginController {
             TokenDTO token = loginServiceImpl.login(loginRequestDto);
             return ResponseEntity.ok(token);
         } catch (Exception e) {
+
+            ResponseDto responseDto = new ResponseDto(401, e.getMessage(), null);
             return ResponseEntity
                     .status(401)
                     .body("Error al iniciar sesión: " + e.getMessage());
