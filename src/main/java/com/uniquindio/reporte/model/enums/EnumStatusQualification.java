14717160efa8b6871fe3baef7dedcac1e418fd3c
julian Naranjo <julianandresnaranjoalzate@gmail.com>
2025-04-13 11:53:58 -0500

    agregate EmunQualification

--- src/main/java/com/uniquindio/reporte/model/enums/EnumStatusQualification.java
+++ src/main/java/com/uniquindio/reporte/model/enums/EnumStatusQualification.java
@@ -1,10 +1,6 @@
 package com.uniquindio.reporte.model.enums;
 
-<<<<<<< Updated upstream
-public enum qualification {
-=======
 public enum EnumStatusQualification {
     ACTIVO,
     ELIMINADO
->>>>>>> Stashed changes
 }
